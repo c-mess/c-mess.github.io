f6c68a9b842b4655872fd29538b56aecf54e6475
Complete Mess <deeowneese1@gmail.com>
2017-05-15 01:26:21 +1100

    Mainpage-exclusive script

--- src/js/main.js
+++ src/js/main.js
@@ -5,32 +5,34 @@ window.onload = function () {
     $('#nav_c').on('click', function () {
         $('#nav_c, #nav_b, nav, main').toggleClass('tg');
     });
-    var titles = {
-        'title': "Cmess' Groovy Place",
-        'phrases': [
-            'Spice must flow!',
-            'Buckle up!',
-            'Pure clocktwork.',
-            'Spend your leaves here!',
-            'Shinji Ikari dislikes our robots.',
-            'Long live the LISP!',
-            'Hop onto my cdr!',
-            'Definitely not a droid you are looking for...',
-            'Art of laze.',
-            'Petrified lizard.',
-            'require("helicopters");',
-            'Commonwealth of neurons.',
-            'Delimited by infinity.',
-            '%tagline%',
-            '01011001 01100101 01110000 00101110',
-            'Production is tend to break',
-            'Unknown reference',
-            'F5 Again, kid.',
-            'A script for main page'
-        ]
+    if (window.location.href == 'https://c-mess.github.io/') {
+        var titles = {
+            'title': "Cmess' Groovy Place",
+            'phrases': [
+                'Spice must flow!',
+                'Buckle up!',
+                'Pure clocktwork.',
+                'Spend your leaves here!',
+                'Shinji Ikari dislikes our robots.',
+                'Long live the LISP!',
+                'Hop onto my cdr!',
+                'Definitely not a droid you are looking for...',
+                'Art of laze.',
+                'Petrified lizard.',
+                'require("helicopters");',
+                'Commonwealth of neurons.',
+                'Delimited by infinity.',
+                '%tagline%',
+                '01011001 01100101 01110000 00101110',
+                'Production is tend to break',
+                'Unknown reference',
+                'F5 Again, kid.',
+                'Script piece for main page exclusively'
+            ]
+        }
+        var max = titles.phrases.length - 1,
+            rand = Math.floor(Math.random() * max),
+            string = titles.title + ' — ' + titles.phrases[rand];
+        $('title').text(string);
     }
-    var max = titles.phrases.length - 1,
-        rand = Math.floor(Math.random() * max),
-        string = titles.title + ' — ' + titles.phrases[rand];
-    $('title').text(string);
 }
