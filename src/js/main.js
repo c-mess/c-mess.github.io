93f38a504accf9c41c9082336f0fb05e301439da
Complete Mess <deeowneese1@gmail.com>
2017-05-14 23:30:05 +1100

    Content styles and so on

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,33 @@
-window.onload = function() {
-    $('#nav_b').on('click', function() {
+window.onload = function () {
+    $('#nav_b').on('click', function () {
         $('#nav_c, #nav_b').toggleClass('tg');
     });
+    var titles = {
+        'title': "Cmess' Groovy Place",
+        'phrases': [
+            'Spice must flow!',
+            'Buckle up!',
+            'Pure clocktwork.',
+            'Spend your leaves here!',
+            'Shinji Ikari dislikes our robots.',
+            'Long live the LISP!',
+            'Hop onto my cdr!',
+            'Definitely not a droid you are looking for...',
+            'Art of laze.',
+            'Petrified lizard.',
+            'require("helicopters");',
+            'Commonwealth of neurons.',
+            'Delimited by infinity.',
+            '%tagline%',
+            '01011001 01100101 01110000 00101110',
+            'Production is tend to break',
+            'Unknown reference',
+            'F5 Again, kid.',
+            'A script for main page'
+        ]
+    }
+    var max = titles.phrases.length - 1,
+        rand = Math.floor(Math.random() * max),
+        string = titles.title + ' — ' + titles.phrases[rand];
+    $('title').text(string);
 }
